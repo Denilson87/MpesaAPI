e1884576207ece2893a3b69df442f51bda723b3c
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-09-14 13:22:59 +0200

    Updating routes

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::apiResource('/transaction','TransactionController');
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
